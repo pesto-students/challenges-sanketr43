5bf57057195199727e514e3e6b69ebc0c23b3cc5
sanket <sanketr43@gmail.com>
2020-09-29 18:48:58 +0530

    alternating-characters exercise solved.

---
+++ js-exercises/alternating-characters/alternatingCharacters.js
@@ -0,0 +1,17 @@
+function alternatingCharacters(arr) {
+  return arr.map((str) => {
+    if (str.length === 1) return 0;
+    let prevChar = str[0];
+    let noOfDeletions = 0;
+    for (let char of str.slice(1, str.length)) {
+      if (char === prevChar) {
+        noOfDeletions += 1;
+      }
+      prevChar = char;
+    }
+
+    return noOfDeletions;
+  });
+}
+
+export { alternatingCharacters };
